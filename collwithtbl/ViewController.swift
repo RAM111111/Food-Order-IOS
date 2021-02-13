39dce11f94dc25df50c0f5e20b0f8b450895edce
RAM111111 <r.r.99@windowslive.com>
2021-02-14 02:00:44 +0300

    Initial Commit

---
+++ collwithtbl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collwithtbl
+//
+//  Created by R on 02/07/1442 AH.
+//  Copyright © 1442 R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
